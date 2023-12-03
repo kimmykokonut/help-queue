85ad27fc36b451d70863742aabec494d0547c496
Kim Robinson <kimmykokonut@gmail.com>
2023-12-03 10:13:07 -0800

    add second ticket

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Ticket from "./Ticket";
 
-function TicketList(){
-  return(
-    <Ticket
-      location="3A"
-      name="Thato and Haley"
-      issue="Firebase will not save record!" />
+function TicketList() {
+  return (
+    <React.Fragment>
+      <Ticket
+        location="3A"
+        name="Thato and Haley"
+        issue="Firebase will not save record!" />
+      <Ticket
+        location="4B"
+        names="Sleater and Kinney"
+        issue="Prop types are throwing an error." />
+    </React.Fragment>
   );
 }
 
