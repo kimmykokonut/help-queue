c8cd843071370f51207ceeeeabf395eef807cb90
Kim Robinson <kimmykokonut@gmail.com>
2023-12-04 20:05:47 -0800

    refactor button to toggle between components

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -16,17 +16,19 @@ class TicketControl extends React.Component {
   }
   render(){
     let currentlyVisibleState = null;
-    let addTicketButton = null;
+    let buttonText = null;
     if (this.state.formVisibleOnPage) {
-      currentlyVisibleState = <NewTicketForm />
+      currentlyVisibleState = <NewTicketForm />;
+      buttonText = "Return to Ticket List";
     } else {
-      currentlyVisibleState = <TicketList />
-      addTicketButton= <button onClick={this.handleClick}>Add ticket</button>
+      currentlyVisibleState = <TicketList />;
+      buttonText = "Add Ticket";
+      
     }
     return (
       <React.Fragment>
         {currentlyVisibleState}
-        {addTicketButton}
+        <button onClick={this.handleClick}>{buttonText}</button>
       </React.Fragment>
     );
   }
