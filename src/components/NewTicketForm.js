57516c68751a46e07f3d19e6125814ecd575ed27
Kim Robinson <kimmykokonut@gmail.com>
2023-12-06 20:41:20 -0800

    add resuable form comp. and add to newticketform

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { v4 } from 'uuid';
+import ReusableForm from './ResusableForm';
 
 function NewTicketForm(props) {
   function handleNewTicketFormSubmission(e) {
@@ -18,20 +19,9 @@ function NewTicketForm(props) {
   }
   return (
     <React.Fragment>
-      <form onSubmit={handleNewTicketFormSubmission}>
-        <input
-          type='text'
-          name='names'
-          placeholder='Pair Names' />
-        <input
-          type='text'
-          name='location'
-          placeholder='Location' />
-        <textarea
-          name='issue'
-          placeholder='Describe your issue' />
-        <button type='submit'>Help!</button>
-      </form>
+      <ReusableForm
+        formSubmissionHandler={handleNewTicketFormSubmission}
+        buttonText="Halp!" />
     </React.Fragment>
   );
 }
