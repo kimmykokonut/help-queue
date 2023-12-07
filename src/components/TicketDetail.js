b9c664bfa583b5e6e448e348cebdcd32f33a432d
Kim Robinson <kimmykokonut@gmail.com>
2023-12-06 20:08:05 -0800

    update TDetail comp to render Selectedticket

--- src/components/TicketDetail.js
+++ src/components/TicketDetail.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function TicketDetail(props) {
+  const { ticket } = props; //obj destructure so no need props.ticket.location
+
   return (
     <React.Fragment>
       <h1>Ticket Detail</h1>
+      <h3>{ticket.location} - {ticket.names}</h3>
+      <p><em>{ticket.issue}</em></p>
       <hr/>
     </React.Fragment>
   );
 }
-
+TicketDetail.propTypes = {
+  ticket: PropTypes.object
+}
 export default TicketDetail;
