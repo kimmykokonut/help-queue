5f1864535433e1dcaae288d07818bc9b92534e0b
Kim Robinson <kimmykokonut@gmail.com>
2023-12-11 20:06:36 -0800

    all 5 rootreducer tests pass

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -12,33 +12,32 @@ describe('rootReducer', () => {
   test('Check that initial state of formVisibleReducer matches root reducer', () => {
     expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null}));
   });
-  //add these tests once the first passes.
-  // test('Should return default state if no action type is recognized', () => {
-  //   expect(rootReducer({}, {type: null})).toEqual({
-  //     mainTicketList: {},
-  //     formVisibleOnPage: false 
-  //   });
-  // });
 
-  // test('Check that ADD_TICKET action works for ticketListReducer and root reducer', () => {
-  //   const action = {
-  //     type: 'ADD_TICKET',
-  //     names: 'Ryan & Aimen',
-  //     location: '4b',
-  //     issue: 'Redux action is not working correctly.', 
-  //     id: 1
-  //   }
-  //   store.dispatch(action);
-  //   expect(store.getState().mainTicketList).toEqual(ticketListReducer(undefined, action));
-  // });
+  test('Should return default state if no action type is recognized', () => {
+    expect(rootReducer({}, {type: null})).toEqual({
+      mainTicketList: {},
+      formVisibleOnPage: false 
+    });
+  });
+
+  test('Check that ADD_TICKET action works for ticketListReducer and root reducer', () => {
+    const action = {
+      type: 'ADD_TICKET',
+      names: 'Ryan & Aimen',
+      location: '4b',
+      issue: 'Redux action is not working correctly.', 
+      id: 1
+    }
+    store.dispatch(action);
+    expect(store.getState().mainTicketList).toEqual(ticketListReducer(undefined, action));
+  });
   // //these tests dispatch an action. rootred shoudl handle the actions by passing into ind. reducers and update state slice
-  // test('Check that TOGGLE_FORM action works for formVisibleReducer and root reducer', () => {
-  //   const action = {
-  //     type: 'TOGGLE_FORM'
-  //   }
-  //   store.dispatch(action);
-  //   expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
-  // });
+  test('Check that TOGGLE_FORM action works for formVisibleReducer and root reducer', () => {
+    const action = {
+      type: 'TOGGLE_FORM'
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
+  });
 
-  
 });
