65bc0cdd58438cd06c3463d375ba02b6c00c9640
Kim Robinson <kimmykokonut@gmail.com>
2023-12-12 19:24:43 -0800

    finished testing and creating action creators

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -7,4 +7,25 @@ describe('Help Queue actions', () => {
       id: 1
     });
   });
+  it('toggleForm should create TOGGLE_FORM action', () => {
+    expect(actions.toggleForm()).toEqual({
+      type: 'TOGGLE_FORM'
+    });
+  });
+  it('addTicket should create ADD_TICKET action', () => {
+    expect(actions.addTicket({
+      names: 'Jo and Jasmine',
+      location: '3E',
+      issue: 'Redux not working!',
+      id: 1
+    })).toEqual({
+      type: 'ADD_TICKET',
+      names: 'Jo and Jasmine',
+      location: '3E',
+      issue: 'Redux not working!',
+      id: 1
+    });
+  });
+
+
 });
--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,18 @@ export const deleteTicket = id => ({
   type: 'DELETE_TICKET',
   id 
 });
+
+export const toggleForm = () => ({
+  type: 'TOGGLE_FORM'
+});
+
+export const addTicket = (ticket) => {
+  const { names, location, issue, id } = ticket;
+  return {
+    type: 'ADD_TICKET',
+    names: names,
+    location: location,
+    issue: issue,
+    id: id
+  }
+}
